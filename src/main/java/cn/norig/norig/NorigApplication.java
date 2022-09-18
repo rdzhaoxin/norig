abf8921ceef141700cfc7918d69c61da6df424d6
zhaoxin <rd.zhaoxin@foxmail.com>
2022-09-18 23:16:17 +0800

    feat(first commit ):

---
+++ src/main/java/cn/norig/norig/NorigApplication.java
@@ -0,0 +1,17 @@
+package cn.norig.norig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *	启动类
+ * @author zhaoxin
+ */
+@SpringBootApplication
+public class NorigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/norig/norig/NorigApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.norig.norig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
