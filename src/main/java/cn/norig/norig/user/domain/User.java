9062e244ffed9cc241faee720c90628eb1e975b0
zhaoxin <rd.zhaoxin@foxmail.com>
2022-10-18 12:27:24 +0800

    test

---
+++ src/main/java/cn/norig/norig/user/domain/User.java
@@ -0,0 +1,6 @@
+package cn.norig.norig.user.domain;/**
+ *
+ * @author zhaoxin
+ * @version 1.0
+ */public class User {
+}
---
+++ src/main/java/cn/norig/norig/user/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package cn.norig.norig.user.repository;/**
+ *
+ * @author zhaoxin
+ * @version 1.0
+ */public interface UserRepository {
+}
